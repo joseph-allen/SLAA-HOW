a91d3f817aa4ec617d6c67edddcc03548bb66124
Joe RN <joseph.allen.1994@hotmail.com>
2020-04-29 21:40:00 +0100

    Add first 7 questions

--- src/App.js
+++ src/App.js
@@ -43,25 +43,27 @@ function App() {
 
           <Switch>
             <Route path="/Q1">
-              <Home question="Question 1" />
+              <Home number={1} question="Question 1. What is sex and love addiction?
+ Read the twelve characteristics of sex and love addiction. Underline any words or concepts you are unfamiliar with and discuss them with your sponsor. Also underline passages that you identify with or that remind you of your own behaviour. Discuss these on your daily outreach calls and also with your sponsor.
+" />
             </Route>
             <Route path="/Q2">
-              <Home question="Question 2" />
+              <Home number={2} question="Question 2. Read Step 1 from the SLAA Basic Text (pp.68-70). Do you identify as a sex addict, a love addict, an anorexic or all of these ? Referring to examples from your life, discuss how you have acted out (or in) in these areas in the past." />
             </Route>
             <Route path="/Q3">
-              <Home question="Question 3" />
+              <Home number={3} question="Question 3. Read the ’40 Questions for Self-Diagnosis’. Tick the behaviours you most identify with. Choose the 10 most relevant and write a few sentences on each using specific examples from your past." />
             </Route>
             <Route path="/Q4">
-              <Home question="Question 4" />
+              <Home number={4} question="Question 4. Read the S.L.A.A literature on anorexia as it relates to S.L.A.A .(sexual, social and emotional anorexia). Do you relate to any of these behaviours? Discuss." />
             </Route>
             <Route path="/Q5">
-              <Home question="Question 5" />
+              <Home number={5} question="Question 5. Read ‘The Withdrawal Experience’ in the S.L.A.A. Basic Text and also the ‘Withdrawal’ pamphlet. Have you ever experienced withdrawal in Relation to sex and love/relationship? (e.g. when a relationship has ended or was terminated)." />
             </Route>
             <Route path="/Q6">
-              <Home question="Question 6" />
+              <Home number={6} question="Question 6. Read what you can on the topic ‘Is it Love or is it Addiction?’ Note anything that is meaningful to you. Discuss and reflect on what you have learnt. Read ‘What is Acting Out?’ and ‘What are Slips?’ from the ‘Questions Beginners Ask’ pamphlet. Have you tried to control your compulsive behaviour in the past? How have you ‘slipped’ and ‘acted out’ after decisions to behave differently?" />
             </Route>
             <Route path="/Q7">
-              <Home question="Question 7" />
+              <Home number={7} question="Question 7. Read the pamphlets ‘Se\ng Bottom Lines’ and ‘Se\ng Top Lines.’ Write a list of the obvious behaviours that you would consider necessary to bottom-line or top-line. Write a list of more subtle behaviours that you suspect may cause you trouble. Discuss these with your sponsor to arrive at bottom-line and/or top line behaviours for the 30 day Step 1,2 and 3 period. Absolute honesty is necessary to combat Sex and Love Addiction." />
             </Route>
           </Switch>
         </div>
@@ -74,7 +76,14 @@ function Home(props) {
   // constructor(props) {
   //   super(props);
   // }
-  return <h2>{props.question}</h2>;
+  return (
+    <div>
+      <Link to={"/Q" + (props.number - 1)}  >{"<"} Question {props.number - 1} </Link>
+      <Link to={"/Q" + (props.number + 1)} >Question {props.number + 1}{">"}</Link>
+      <h2>{props.question}</h2>
+    </div>
+  )
+    ;
 }
 
 export default App;
